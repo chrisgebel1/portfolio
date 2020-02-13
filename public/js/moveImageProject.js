d35b9ab77558d7101c0a90d341d9a947571385ce
Christophe GEBELIN <gebelinchristophe@gmail.com>
2020-02-13 16:36:22 +0100

    modification des projets sur la hoempage

---
+++ public/js/moveImageProject.js
@@ -0,0 +1,82 @@
+$(document).ready( function(){
+    "use strict";
+
+    // $(".container-delete").mouseenter( function (){
+    //     $(".delete-img-btn-legend",
+    //         // this).stop().animate({"top":"0%",}, 200, 'swing');
+    //         this).stop().fadeTo(50, 1);
+    // });
+    // $(".container-delete").mouseleave( function (){
+    //     $(".delete-img-btn-legend",
+    //         // this).stop().animate({"top":"100%",}, 200, 'swing');
+    //         this).stop().fadeTo(50, 0);
+    // });
+
+    $(document).on('click', '.order-move', function(e){
+        e.preventDefault();
+
+        // console.log($(this)[0].dataset.order);
+        // console.log($(this)[0].offsetParent.offsetParent.children[0].src);
+        const $params = $(this)[0].dataset.order.split('-');
+        const $p = $params[0];
+        const $i = $params[1];
+        const $o = $params[2];
+
+        const $regex = /(?:(\w+\.\w+))$/gm;
+        const $pathImg = $(this)[0].offsetParent.offsetParent.children[0].src;
+        const $img = (RegExp($regex).exec($pathImg))[0];
+
+        // console.log($params);
+
+        const $responseConfirm = confirm('Voulez-vous vraiment modifier l\'odre ?');
+        /* response => true (OK) ou false (ANNULER) */
+
+        if ($responseConfirm) {
+            // console.log($paramProject);
+            // console.log($paramIndexImg);
+            // console.log($paramOrder);
+
+            $.ajax({
+                url:$url,
+                method:"POST",
+                data:{
+                    project: $p,
+                    img: $img,
+                    indexImg: $i,
+                    order: $o
+                },
+                dataType: "json",
+                success:function(data)
+                {
+                    if ( data.status === 'success' ) {
+                        // console.log(data);
+                        $(".project-images").replaceWith(data.html);
+
+                    } else if ( data.status === 'error' ) {
+                        console.log(data);
+                    }
+
+                },
+                error:function(x,e) {
+                    if (x.status==0) {
+                        alert('You are offline!!\n Please Check Your Network.');
+                    } else if(x.status==404) {
+                        alert('Requested URL not found.');
+                    } else if(x.status==500) {
+                        alert('Internel Server Error.');
+                    } else if(e=='parsererror') {
+                        alert('Error.\nParsing JSON Request failed.');
+                    } else if(e=='timeout'){
+                        alert('Request Time out.');
+                    } else {
+                        alert('Unknow Error.\n'+x.responseText);
+                    }
+                }
+            });
+
+        }
+
+    });
+
+
+});
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -26,6 +26,62 @@ class IndexController extends AbstractController
         $types = $typeRepository->findAll();
         $categories = $categoryRepository->findAll();
 
+        // sélectionner aléatoirement au minimum 3 projets de chaque type si possible
+        // il faut un total de 6 projets à afficher
+
+        $maxProjectsByType = 3; // nbre de projet à afficher par type
+        $totalProjects = $maxProjectsByType * ( count($types) ); // nbre total de projets à afficher
+        $selectProjects = []; // les projets à afficher
+        $typProjects = []; // sélection des projets par type avec une limite
+
+        if ( count($types) > 0 ) {
+            foreach ( $types as $type ) {
+                $p = $projectRepository->findBy(
+                    [ 'type'    => $type->getId() ],
+                    [ 'id'      => 'DESC'  ],
+                    $totalProjects
+                );
+                $typProjects[$type->getId()] = $p;
+            }
+
+            while ( count($selectProjects) < $totalProjects ) {
+
+                foreach ( $typProjects as $typProject ) { // $typVal tous les projets du type
+
+                    $typProjectsById = []; // contient les projets du même type indexés par l'ID du projet
+                    foreach ( $typProject as $project ) {
+                        $typProjectsById[$project->getId()] = $project;
+                    }
+
+                    if ( count($typProject) > 0 ) {
+                        $randomProject = array_slice($typProjectsById, rand(0, count($typProjectsById)-1), 1)[0]; // sélection aléatoire d'un projet
+                        $exist = false;
+                        foreach ($selectProjects as $p) { // on vérifie qu'on ne l'a pas déjà sélectionné
+                            if ($randomProject->getId() == $p->getId()) {
+                                $exist = true;
+                                break;
+                            }
+                        }
+
+                        if (!$exist) { // si on ne l'a pas déjà sélectionné on l'enregistre dans les projets à afficher
+                            foreach($typProject as $project) {
+                                if ($project->getId() == $randomProject->getId()) {
+                                    $selectProjects[] = $randomProject;
+                                    break;
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            shuffle($selectProjects); // mélange l'ordre des projets sélectionnés
+            $projects = $selectProjects;
+        }
+
+
+
         return $this->render(
             'index/index.html.twig',
             [
@@ -40,6 +96,7 @@ class IndexController extends AbstractController
      * @param Request $request
      * @param ProjectRepository $projectRepository
      * @Route("/search-result")
+     * @return JsonResponse
      */
     public function searchProject(Request $request, ProjectRepository $projectRepository)
     {
